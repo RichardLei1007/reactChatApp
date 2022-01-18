a9fdc180104ac4ab379c347bd1e53368d0f60dfc
RichardLei1007 <richardleihh@gmail.com>
2022-01-18 14:33:56 -0800

    update

--- src/App.js
+++ src/App.js
@@ -4,20 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header className = "header">
+      <img className = "logo"></img>
+      <span className = "title">Test App</span>
+    </header>
+
+    <footer className = "footer">
+      <input className = "input"></input>
+      <button className = "button">↑</button>
+    </footer>
     </div>
   );
 }
