3f550ed27f9d5e01ed65473dbc51c9c4719c0df8
RichardLei1007 <richardleihh@gmail.com>
2022-01-25 15:18:30 -0800

    Update Jan 25

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import TextInput from './TextInput';
+import { useState } from "react";
+import Message from './Message'
 
 function App() {
+  const [messages, setMessages] = useState([]);
+  function sendMessage(msg) {
+    const newMessage = {
+      msg,
+      time: Date.now(),
+      user: "Me",
+    };
+    setMessages([newMessage, ...messages]);
+  }
+  console.log(messages);
   return (
     <div className="App">
     <header className = "header">
       <img className = "logo"></img>
       <span className = "title">Chat Bot 3000</span>
     </header>
-
-    <footer className = "footer">
-      <input className = "input"></input>
-      <button className = "button">↑</button>
-      <button className = "img"></button>
-      <button className = "emoji"></button>
-    </footer>
+    <div className = "messages"> 
+      {messages.map((msg) => {
+        return <Message {...msg}></Message>;
+      })}
+      </div>
+    <TextInput sendMessage = {sendMessage} />
     </div>
   );
 }
---
+++ src/TextInput.js
@@ -0,0 +1,26 @@
+import "./TextInput.css";
+import { useState } from "react";
+
+function TextInput(props) {
+    const [text, setText] = useState("")
+
+    function send() {
+        props.sendMessage(text);
+        setText("");
+    }
+
+    function onKeyPress(e) {
+        if (e.key === "Enter") {
+          send();
+        }
+    }
+
+    return (
+        <footer className = "footer">
+            <input className = "input" value = {text} onChange = {(e) => setText(e.target.value)} onKeyPress={onKeyPress}></input>
+            <button className = "button" onClick = {send}>↑</button>
+        </footer>
+    );
+}
+
+export default TextInput;
